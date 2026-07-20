9ede211ab5067c62378fd75653d5feb19fb97662
Shotjr <johan385@gmail.com>
2026-07-20 17:54:58 +0200

    Update test_auth.py

--- tests/unit/test_auth.py
+++ tests/unit/test_auth.py
@@ -1,1 +1,4 @@
-
+def test_login_success(client):
+    response = client.post("/login", data={"user":"juan","password":"123"})
+    assert response.status_code == 200
+    assert b"Bienvenido" in response.data
