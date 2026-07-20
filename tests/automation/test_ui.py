4c1b1a435729de489868123b744c54da4352ae84
Shotjr <johan385@gmail.com>
2026-07-20 17:55:41 +0200

    Update test_ui.py

--- tests/automation/test_ui.py
+++ tests/automation/test_ui.py
@@ -1,1 +1,13 @@
+from selenium import webdriver
 
+def test_create_task_ui():
+    driver = webdriver.Chrome()
+    driver.get("http://localhost:5000/login")
+    driver.find_element("id","username").send_keys("juan")
+    driver.find_element("id","password").send_keys("123")
+    driver.find_element("id","login-btn").click()
+    driver.find_element("id","new-task").click()
+    driver.find_element("id","title").send_keys("Tarea automática")
+    driver.find_element("id","save-btn").click()
+    assert "Tarea automática" in driver.page_source
+    driver.quit()
